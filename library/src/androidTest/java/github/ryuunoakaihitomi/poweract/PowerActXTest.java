a7e5ef1fd994c0bdc6a2c8e6ad103caea0a31b83
ryuunoakaihitomi <22442525+ryuunoakaihitomi@users.noreply.github.com>
2020-09-22 09:50:59 +0800

    libsu for test
    Library
    [Test] PowerActXTest: Import libsu.

--- library/src/androidTest/java/github/ryuunoakaihitomi/poweract/PowerActXTest.java
+++ library/src/androidTest/java/github/ryuunoakaihitomi/poweract/PowerActXTest.java
@@ -3,7 +3,11 @@ package github.ryuunoakaihitomi.poweract;
 import androidx.test.filters.FlakyTest;
 import androidx.test.filters.LargeTest;
 
+import com.topjohnwu.superuser.Shell;
+
 import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import github.ryuunoakaihitomi.poweract.internal.pa.PaReceiver;
@@ -13,11 +17,22 @@ import github.ryuunoakaihitomi.poweract.test.BaseTest;
 import github.ryuunoakaihitomi.poweract.test.LockScreenTest;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 
 @FlakyTest(detail = "These cases require root privilege. You must test it manually.")
 @LargeTest
 public final class PowerActXTest extends BaseTest {
+
+    @BeforeClass
+    public static void enableVerboseLog() {
+        Shell.enableVerboseLogging = true;
+    }
+
+    @Before
+    public void checkRoot() {
+        assertTrue(Shell.rootAccess());
+    }
 
     @After
     public void checkExposedComponents() {
