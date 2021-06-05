7232869ead0987c9d6737ff337af26cba8f936ce
ryuunoakaihitomi <22442525+ryuunoakaihitomi@users.noreply.github.com>
2021-06-06 01:06:59 +0800

    More reliable wearable env detection solution.
    Demo app
    [Refactor] More reliable wearable env detection solution.

--- app/src/main/java/demo/power_act/util/Utils.java
+++ app/src/main/java/demo/power_act/util/Utils.java
@@ -1,18 +1,15 @@
 package demo.power_act.util;
 
+import android.app.UiModeManager;
 import android.content.Context;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.util.Log;
+import android.content.res.Configuration;
+import android.os.Build;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 public class Utils {
-
-    private static final String TAG = "Utils";
 
     private Utils() {
     }
@@ -22,13 +19,21 @@ public class Utils {
     }
 
     public static boolean isAndroidWearOS(Context context) {
-        try {
-            PackageManager pm = context.getPackageManager();
-            PackageInfo info = pm.getPackageInfo("com.google.android.wearable.app", 0);
-            Log.i(TAG, "isAndroidWearOS: App Version:" + info.versionName);
-            return (info.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) > 0;
-        } catch (PackageManager.NameNotFoundException ignored) {
-            return false;
+        final int sdk = Build.VERSION.SDK_INT;
+        if (sdk >= Build.VERSION_CODES.KITKAT_WATCH) {
+            UiModeManager uiModeManager = sdk >= Build.VERSION_CODES.M
+                    ? context.getSystemService(UiModeManager.class)
+                    : (UiModeManager) context.getSystemService(Context.UI_MODE_SERVICE);
+            return uiModeManager.getCurrentModeType() == Configuration.UI_MODE_TYPE_WATCH;
         }
+        return false;
+//        try {
+//            PackageManager pm = context.getPackageManager();
+//            PackageInfo info = pm.getPackageInfo("com.google.android.wearable.app", 0);
+//            Log.i(TAG, "isAndroidWearOS: App Version:" + info.versionName);
+//            return (info.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) > 0;
+//        } catch (PackageManager.NameNotFoundException ignored) {
+//            return false;
+//        }
     }
 }
